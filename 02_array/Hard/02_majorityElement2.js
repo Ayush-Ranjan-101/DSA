17c09512198cb871b394935791beee865fa73144
Ayush-Ranjan-101 <ayushranjan4542@gmail.com>
2026-02-13 12:26:03 +0530

    1 mid 1 hard

---
+++ 02_array/Hard/02_majorityElement2.js
@@ -0,0 +1,67 @@
+// leetcode - 229
+
+// const nums = [1, 2, 1, 1, 3, 2];
+const nums = [1, 2, 1, 1, 3, 2, 2];
+
+const majorityElement = function  (nums) {
+    let count1 = 0, count2 = 0;
+    let ele1 = -Infinity, ele2 = -Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (count1 === 0 && ele2 !== nums[i]) {
+            count1++;
+            ele1 = nums[i];
+        } else if (count2 === 0 && ele1 !== nums[i]) {
+            count2++;
+            ele2 = nums[i];
+        } else if (nums[i] === ele1) {
+            count1++;
+        } else if (nums[i] === ele2) {
+            count2++;
+        } else {
+            count1--;
+            count2--;
+        }
+    }
+
+    const res = [], bar = Math.floor(nums.length/3);
+    count1 = count2 = 0;
+
+    // We recheck as the traversel above just shows the potentail majority elements
+    // test case for the above statement : [1,2,3,4,5,6];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === ele1) count1++;
+        else if (nums[i] === ele2) count2++;
+    }
+
+    if (count1 > bar) res.push(ele1);
+    if (count2 > bar) res.push(ele2);
+
+    return res;
+}
+
+// Better apporach
+// const majorityElement = function  (nums) {
+//     const freq = new Map();
+//
+//     const res = [], bar = Math.floor(nums.length/3) + 1;
+//     for (let i = 0; i < nums.length; i++) {
+//         const key = nums[i];
+//         freq.set(key, (freq.get(key) || 0) + 1);
+//
+//         if (freq.get(key) === bar) {
+//             res.push(key);
+//         }
+//
+//         if (res.length === 2) {
+//             break;
+//         }
+//     }
+//
+//     return res;
+// }
+
+// Time complexity : O(n)
+// Space complexity : O(n)
+
+console.log(majorityElement(nums));
---
+++ 02_array/Medium/13_mtrixSpiral.js
@@ -0,0 +1,45 @@
+// leetcode - 54
+
+const matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+const spiralOrder = function  (matrix) {
+    let res = [];
+
+    let top = 0, left = 0;
+    let bottom = matrix.length -1, right = matrix[0].length -1;
+
+    while (top <= bottom && left <= right) {
+
+        // Traverse left -> right along the top row
+        for (let i = left; i <= right; i++) {
+            res.push(matrix[top][i]);
+        }
+        top++;
+
+        // Traverse top -> bottom along the right column
+        for (let i = top; i <= bottom; i++) {
+            res.push(matrix[i][right]);
+        }
+        right--;
+
+        // Traverse right -> left along the bottom row
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                res.push(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        // Traverse bottom -> top along the left column
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                res.push(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return res;
+}
+
+console.log(spiralOrder(matrix));
